509c45c520b2dd32e8875a39a9ad9d1f3905625c
Adouni <ghisbernis@yahoo.fr>
2016-02-02 18:19:12 +0100

    Revert "Liste course"
    This reverts commit 2b4dd6437a0b5928c42ba7051ec34f25c218d018.

--- test-mvc/src/main/java/lan/viveris/testmvc/controller/BonjourController.java
+++ test-mvc/src/main/java/lan/viveris/testmvc/controller/BonjourController.java
@@ -2,16 +2,17 @@ package lan.viveris.testmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/bonjour/{personne}")
+@RequestMapping("/bonjour")
 public class BonjourController {
-	@RequestMapping(method = RequestMethod.GET)
+
+    @RequestMapping(method = RequestMethod.GET)
     public String afficherBonjour(final ModelMap pModel, 
-                @PathVariable(value="personne") final String pPersonne) {
+                @RequestParam(value="personne", defaultValue="toto") final String pPersonne) {
 
         pModel.addAttribute("personne", pPersonne);
         return "bonjour";
--- test-mvc/src/main/java/lan/viveris/testmvc/dao/IListeCoursesDAO.java
+++
@@ -1,9 +0,0 @@
-package lan.viveris.testmvc.dao;
-
-import java.util.List;
-
-import lan.viveris.testmvc.bean.Course;
-
-public interface IListeCoursesDAO {
-    List<Course> rechercherCourses();
-}
