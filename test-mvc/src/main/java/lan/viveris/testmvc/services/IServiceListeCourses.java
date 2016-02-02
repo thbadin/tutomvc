bff506d2e63a64417453d629d74d190bd8fd4d63
Thomas <badin.thomas@gmail.com>
2016-02-02 17:46:35 +0100

    Essais revert change

--- test-mvc/src/main/java/lan/viveris/testmvc/services/IServiceListeCourses.java
+++
@@ -1,9 +0,0 @@
-package lan.viveris.testmvc.services;
-
-import java.util.List;
-
-import lan.viveris.testmvc.bean.Course;
-
-public interface IServiceListeCourses {
-    List<Course> rechercherCourses();
-}
